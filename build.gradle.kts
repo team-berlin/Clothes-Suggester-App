2463bdcc573be398c052569165177df0de7466de
ahmad <ahmad@test.com>
2025-05-05 15:17:58 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("org.jetbrains.kotlinx.kover") version "0.9.1"
+}
+
+group = "com.berlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.insert-koin:koin-core:4.0.4")
+    implementation ("org.jetbrains.kotlinx:kotlinx-datetime:0.6.2")
+    implementation("com.opencsv:opencsv:5.7.1")
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("io.mockk:mockk:1.14.0")
+    testImplementation("com.google.truth:truth:1.4.4")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+}
+
+kover {
+    reports {
+        filters {
+            excludes {
+                classes(
+                    "**.model.**",
+                    "**.di.**",
+                    "**.exception.**",
+                )
+            }
+        }
+
+        total {
+            verify {
+                rule {
+                    minBound(80)
+                }
+            }
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks.named("check") {
+    dependsOn(tasks.koverVerify)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(23)
+}
